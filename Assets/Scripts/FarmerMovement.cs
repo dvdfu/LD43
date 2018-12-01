9f9507640829bce572f0c620aafe6bff2c4197bf
Hamdan Javeed <hamdanjaveed@Hamdans-MacBook.local>
2018-11-30 23:51:25 -0800

    Add collision and better movement

--- Assets/Scripts/FarmerMovement.cs
+++ Assets/Scripts/FarmerMovement.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChickenMovement : MonoBehaviour {
-	const float speed = 100.0f;
+public class FarmerMovement : MonoBehaviour {
+	const float speed = 80.0f;
 	Rigidbody2D body;
 
 	void Start () {
@@ -12,10 +12,10 @@ public class ChickenMovement : MonoBehaviour {
 	
 	void FixedUpdate () {
 		Vector2 move = Vector2.zero;
-		if (Input.GetKey(KeyCode.A)) move.x--;
-		if (Input.GetKey(KeyCode.D)) move.x++;
-		if (Input.GetKey(KeyCode.W)) move.y++;
-		if (Input.GetKey(KeyCode.S)) move.y--;
+		if (Input.GetKey(KeyCode.LeftArrow)) move.x--;
+		if (Input.GetKey(KeyCode.RightArrow)) move.x++;
+		if (Input.GetKey(KeyCode.UpArrow)) move.y++;
+		if (Input.GetKey(KeyCode.DownArrow)) move.y--;
 
 		move = move.normalized * Time.deltaTime * speed;
 		body.MovePosition(body.position + move);
