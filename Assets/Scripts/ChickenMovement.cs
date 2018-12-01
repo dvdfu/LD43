c1a9b1ad0cbb2c9d2d26e955d56a659805bd7642
Hamdan Javeed <hamdanjaveed@Hamdans-MacBook.local>
2018-11-30 23:27:27 -0800

    Add chicken and farmer prefabs

---
+++ Assets/Scripts/ChickenMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenMovement : MonoBehaviour {
+	float speed = 100.0f;
+
+	void Start () {
+		
+	}
+	
+	void FixedUpdate () {
+		Vector2 move = Vector2.zero;
+		if (Input.GetKey(KeyCode.A)) {
+			move.x--;
+		}
+		if (Input.GetKey(KeyCode.D)) {
+			move.x++;
+		}
+		if (Input.GetKey(KeyCode.W)) {
+			move.y++;
+		}
+		if (Input.GetKey(KeyCode.S)) {
+			move.y--;
+		}
+		move = move.normalized * Time.deltaTime * speed;
+
+		transform.Translate(move.x, move.y, 0);
+	}
+}
