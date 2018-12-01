352cfeaa2a7cb717d0ca7773f671d36fc6940faa
dvdfu <davidf1212@gmail.com>
2018-12-01 00:16:17 -0800

    Sprite sorting

---
+++ Assets/Scripts/DepthSort.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DepthSort : MonoBehaviour {
+    SpriteRenderer sprite;
+
+    void Awake() {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    void Update() {
+        sprite.sortingOrder = Mathf.FloorToInt(-transform.position.y);
+    }
+}
