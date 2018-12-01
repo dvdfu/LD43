4f23c0832ea103a01eca893e5300f7b5e345e3f4
dvdfu <davidf1212@gmail.com>
2018-12-01 02:19:06 -0800

    Tween and simple animation classes

---
+++ Assets/Scripts/SimpleAnimation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleAnimation : MonoBehaviour {
+    [SerializeField] SpriteRenderer spriteRenderer;
+    [SerializeField] Sprite[] frames;
+    [SerializeField] float frameDuration = 0.1f;
+    [SerializeField] bool looping = true;
+    [SerializeField] bool autoStart = true;
+
+    Tween tween;
+
+    public void StartAnimation() {
+        spriteRenderer.enabled = true;
+        tween.Start(frameDuration,
+            (float x) => {
+                int index = Mathf.FloorToInt(x * frames.Length);
+                spriteRenderer.sprite = frames[index];
+            }, () => {
+                if (looping) {
+                    StartAnimation();
+                } else {
+                    spriteRenderer.enabled = false;
+                }
+            }
+        );
+    }
+
+    void Awake() {
+        tween = new Tween(this);
+    }
+
+    void Start() {
+        if (autoStart) {
+            StartAnimation();
+        } else {
+            spriteRenderer.enabled = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Tween.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tween {
+    public delegate void OnUpdate(float progress);
+    public delegate void OnFinish();
+
+    MonoBehaviour mb;
+    Coroutine coroutine;
+
+    public Tween(MonoBehaviour mb) {
+        this.mb = mb;
+    }
+
+    public void Start(float duration, OnUpdate onUpdate, OnFinish onFinish) {
+        Stop();
+        mb.StartCoroutine(Animate(duration, onUpdate, onFinish));
+    }
+
+    public void Stop() {
+        if (coroutine != null) {
+            mb.StopCoroutine(coroutine);
+            coroutine = null;
+        }
+    }
+
+    IEnumerator Animate(float duration, OnUpdate onUpdate, OnFinish onFinish) {
+        float t = 0;
+        while (t < duration) {
+            onUpdate(t / duration);
+            t += Time.deltaTime;
+            yield return null;
+        }
+        onFinish();
+    }
+}
