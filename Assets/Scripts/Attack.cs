32edc8e04fea0dab49ec74378910d83746d29fca
Hamdan Javeed <hamdan@hamdanjaveed.com>
2018-12-01 01:47:29 -0800

    Add attack script

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour {
+	[SerializeField] float attackHoldTheshold = 0.25f;
+	[SerializeField] float closeAttackDuration = 1f;
+	[SerializeField] Collider2D weaponCollider;
+	[SerializeField] KeyCode attackKey = KeyCode.RightShift;
+
+	Timer timer;
+	Timer closeAttackTimer;
+
+	Coroutine attackCoroutine;
+
+	void Start() {
+		timer = new Timer(closeAttackDuration);
+		closeAttackTimer = new Timer(closeAttackDuration);
+	}
+
+	void Update () {
+		timer.update(Time.deltaTime);
+
+		if (Input.GetKeyUp(attackKey) && timer.stop() < attackHoldTheshold && attackCoroutine == null) {
+			attackCoroutine = StartCoroutine(closeAttack());
+		}
+	}
+
+	IEnumerator closeAttack() {
+		closeAttackTimer.reset();
+		while(!closeAttackTimer.isDone()) {
+			yield return null;
+			closeAttackTimer.update(Time.deltaTime);
+		}
+
+		attackCoroutine = null;
+	}
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Timer {
+	bool countUp = true;
+	float timeElapsed;
+	float initialValue;
+
+	public Timer(float startTime = 0f) {
+		timeElapsed = startTime;
+		countUp = startTime == 0;
+		initialValue = startTime;
+	}
+
+	public void start() {
+		// Calling this function makes you feel good
+	}
+
+	public void update(float dt) {
+		if (countUp) timeElapsed += dt;
+		else timeElapsed -= dt;
+	}
+
+	// Only for count down timers
+	public bool isDone() {
+		return timeElapsed <= 0f;
+	}
+
+	public void reset() {
+		timeElapsed = initialValue;
+	}
+
+	public float stop() {
+		float ret = timeElapsed;
+		reset();
+		return ret;
+	}
+}
