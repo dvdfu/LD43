b150ca52edc99846ed227561b3d2538297a8c4bb
dvdfu <davidf1212@gmail.com>
2018-12-01 22:25:17 -0800

    Axe throw ignores own player collisions and stuns

--- Assets/Scripts/Farmer.cs
+++ Assets/Scripts/Farmer.cs
@@ -3,29 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Farmer : Player {
+    const float STUN_DURATION = 0.8f;
+
     [SerializeField] Score.PlayerID id;
-    [SerializeField] float attackHoldTheshold = 0.25f;
     [SerializeField] float attackCooldown = 0.5f;
-    [SerializeField] float axeSwingAttackDuration = 0.1f;
     [SerializeField] float axeThrowSlowdown = 0.5f;
 
 	[SerializeField] KeyCode attackKey = KeyCode.RightShift;
 	[SerializeField] GameObject axeThrowPrefab;
     [SerializeField] GameObject axeSwingPrefab;
+    [SerializeField] GameObject stunEffect;
 
     bool hasAxe;
+    bool isStunned;
 	Vector2 previousDirection;
     Timer attackCooldownTimer;
+    Tween stunTween;
 
 	protected override void Awake() {
         base.Awake();
         hasAxe = true;
+        isStunned = false;
         attackCooldownTimer = new Timer(attackCooldown, true);
 		previousDirection = Vector2.right;
+        stunTween = new Tween(this);
 	}
 
 	void Update() {
-        if (hasAxe) {
+        attackCooldownTimer.update(Time.deltaTime);
+        if (hasAxe && !isStunned) {
             if (Input.GetKeyDown(attackKey)) {
                 if (attackCooldownTimer.isDone()) {
                     speed = maxSpeed * axeThrowSlowdown;
@@ -36,20 +42,41 @@ public class Farmer : Player {
                 attackCooldownTimer.start();
                 GameObject axeThrow = Instantiate(axeThrowPrefab, transform.position, Quaternion.identity);
                 axeThrow.GetComponent<AxeThrow>().Throw(id, previousDirection);
+                Physics2D.IgnoreCollision(
+                    GetComponent<Collider2D>(),
+                    axeThrow.GetComponent<Collider2D>()
+                );
             }
         }
-        
+
 		Vector2 move = GetMoveVector();
 		if (move != Vector2.zero) previousDirection = move;
 	}
 
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("Weapon") &&
+            !isStunned &&
+            collision.gameObject.GetComponent<AxeThrow>().GetID() != id) {
+                
+            stunEffect.SetActive(true);
+            stunEffect.GetComponent<SimpleAnimation>().PlayLoop();
+            isStunned = true;
+            canMove = false;
+            stunTween.Start(STUN_DURATION, (float progress) => {}, () => {
+                stunEffect.SetActive(false);
+                isStunned = false;
+                canMove = true;
+            });
+        }
+    }
+
     void OnTriggerStay2D(Collider2D other) {
-        if (other.gameObject.tag == "AxePickup" && !hasAxe) {
+        if (other.gameObject.CompareTag("AxePickup") && !hasAxe) {
             hasAxe = true;
             Destroy(other.gameObject);
-        } else if (other.gameObject.tag == "Drumstick") {
+        } else if (other.gameObject.CompareTag("Drumstick")) {
             Score.instance.CollectPoint(id);
             Destroy(other.gameObject);
-        }
+        } 
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,6 +18,7 @@ public class GameManager : MonoBehaviour {
 
 	void Start() {
 		SpawnChicken();
+		SpawnChicken();
 	}
 
 	public void GetInstance() {
